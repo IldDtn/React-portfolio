e4b453dbee7e526db8ed106990985555ec639884
Ildi Dutton <ildiko.dutton@yahoo.com>
2023-02-06 22:26:31 +0000

    picture added

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -3,7 +3,7 @@
 function Landing () {
     return (
         <div id='content'>
-         <p id='image'>image</p>
+         <p id='image'></p>
          <p id='empty'></p>
          
          <p class='brandStatement'> Ildiko Dutton <br/> Fullstack Web Developer <br/> Lorem ipsum dolor sit amet consectetur. Risus tempus odio fermentum suspendisse duis. Quam odio quam tristique montes ut egestas nulla at. Laoreet nibh ac quis vitae. Volutpat etiam et etiam congue morbi ut non</p>
