c2f6bd5dccb34e4592c5a306045bed2d659dc4c4
Ildi Dutton <ildiko.dutton@yahoo.com>
2023-02-13 15:59:13 +0000

    contact form updated

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,4 +1,19 @@
+import {useState} from 'react'
+
 function Contact () {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message:''
+  })
+
+  const handleChange = (event) => {
+      setFormData({
+         ...formData,
+         [event.target.name]: event.target.value
+      });
+  }
+
   const handleSubmit = (event) => {
      event.preventDefault();
      console.log('submitted');
@@ -6,11 +21,12 @@ function Contact () {
     return (
         <>
           <h1 id='contactMe'> Contact Me</h1>
+          <p> {formData.email}</p>
 
           <form onSubmit={handleSubmit}>
-            <input type='text' placeholder="Enter your name" />
-            <input type='text' placeholder="Enter your email address" />
-            <textarea placeholder="Enter your message" cols='30' rows='10'> </textarea>
+            <input name='name' onChange={handleChange} type='text' placeholder="Enter your name" />
+            <input name='email' onChange={handleChange} type='text' placeholder="Enter your email address" />
+            <textarea name='message'onChange={handleChange} placeholder="Enter your message" cols='30' rows='10'> </textarea>
             <button id='submit'> Submit </button>
           </form>
         
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -3,7 +3,7 @@
 function Landing () {
     return (
         <div id='content'>
-         <p id='image'></p>
+         <p id='imageMe'></p>
          <p id='empty'></p>
          
          <p class='brandStatement'> <span id='myName'> Ildiko Dutton</span> <br/> <span id='newTitleIldi'> Fullstack Web Developer</span> <br/> Lorem ipsum dolor sit amet consectetur. Risus tempus odio fermentum suspendisse duis. Quam odio quam tristique montes ut egestas nulla at. Laoreet nibh ac quis vitae. Volutpat etiam et etiam congue morbi ut non</p>
