1724c76502882dbbf1a517e15a43445aa8051f01
Ildi Dutton <ildiko.dutton@yahoo.com>
2023-02-13 16:26:35 +0000

    contact form changed

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,39 +1,55 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
-function Contact () {
+function Contact() {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
-    message:''
+    message: '',
+    access_key: '3722bfae-a9b8-4b93-9788-5bbfa4a90d90'
   })
 
   const handleChange = (event) => {
-      setFormData({
-         ...formData,
-         [event.target.name]: event.target.value
-      });
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
   }
 
   const handleSubmit = (event) => {
-     event.preventDefault();
-     console.log('submitted');
-  };
-    return (
-        <>
-          <h1 id='contactMe'> Contact Me</h1>
-          <p> {formData.email}</p>
 
-          <form onSubmit={handleSubmit}>
-            <input name='name' onChange={handleChange} type='text' placeholder="Enter your name" />
-            <input name='email' onChange={handleChange} type='text' placeholder="Enter your email address" />
-            <textarea name='message'onChange={handleChange} placeholder="Enter your message" cols='30' rows='10'> </textarea>
-            <button id='submit'> Submit </button>
-          </form>
-        
-        
-        
-        </>
-    );
+    event.preventDefault();
+
+    const json = JSON.stringify(formData);
+
+
+    fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: json
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  };
+  return (
+    <>
+      <h1 id='contactMe'> Contact Me</h1>
+      <p> {formData.email}</p>
+
+      <form onSubmit={handleSubmit}>
+        <input name='name' onChange={handleChange} type='text' placeholder="Enter your name" />
+        <input name='email' onChange={handleChange} type='text' placeholder="Enter your email address" />
+        <textarea name='message' onChange={handleChange} placeholder="Enter your message" cols='30' rows='10'> </textarea>
+        <button id='submit'> Submit </button>
+      </form>
+
+
+
+    </>
+  );
 }
 
 export default Contact;
