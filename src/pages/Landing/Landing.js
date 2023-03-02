e6ab61bbbad4d10088440bc54909269c0ff568df
Ildi Dutton <ildiko.dutton@yahoo.com>
2023-03-02 14:42:55 +0000

    styling

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,4 +1,4 @@
-
+import { NavLink } from 'react-router-dom';
 
 function Landing () {
     return (
@@ -6,8 +6,8 @@ function Landing () {
          <p id='imageMe'></p>
          <p id='empty'></p>
          
-         <p class='brandStatement'> <span id='myName'> Ildiko Dutton</span> <br/> <span id='newTitleIldi'> Fullstack Web Developer</span> <br/> Lorem ipsum dolor sit amet consectetur. Risus tempus odio fermentum suspendisse duis. Quam odio quam tristique montes ut egestas nulla at. Laoreet nibh ac quis vitae. Volutpat etiam et etiam congue morbi ut non</p>
-
+         <p class='brandStatement'> <span id='myName'> Ildiko Dutton</span> <br/> <span id='newTitleIldi'> Fullstack Web Developer</span> <br/> Lorem ipsum dolor sit amet consectetur. Risus tempus odio fermentum suspendisse duis. Quam odio quam tristique montes ut egestas nulla at. Laoreet nibh ac quis vitae. Volutpat etiam et etiam congue morbi ut non jsjdhdh <span id='cta'> <NavLink to="/about"> Find out more </NavLink> </span> </p>
+         
 
          
         
